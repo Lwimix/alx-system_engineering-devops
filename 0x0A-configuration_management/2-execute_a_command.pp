de046145076c59d86dc649cf2b48f1502e98da9c
Lwimix <martilwinx@gmail.com>
2023-11-24 14:29:58 -0800

    Clear

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,6 @@
 # This script executes pkill on process killmenow
-exec { 'pkill_killmenow':
-command => 'pkill -9 killmenow'
+exec { 'killmenow':
+  command  => '/usr/bin/pkill -9 killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
 }
