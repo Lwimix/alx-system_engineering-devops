f5f823ef488ff6fea56227754453ee40b0c824c5
Lwimix <martilwinx@gmail.com>
2023-11-25 01:39:40 +0300

    Did it

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,7 @@
 # This script creates a file in /tmp specifying the owner and permissions
-$verbose = nil
 file { '/tmp/school':
-<<<<<<< HEAD
-ensure  => present,
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => "I love puppet"
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet',
 }
