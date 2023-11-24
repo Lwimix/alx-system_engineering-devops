369a112d6db055fe6c3ca1a04b162e4c154ed488
Lwimix <martilwinx@gmail.com>
2023-11-24 14:17:03 -0800

    To do 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 $verbose = nil
 file { '/tmp/school':
   ensure  => file,
+  content => 'I love puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
 }
