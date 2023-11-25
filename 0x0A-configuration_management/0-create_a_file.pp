c4738acd5109f409bb08d38cf5ec35371d9f1203
Lwimix <martilwinx@gmail.com>
2023-11-24 16:25:58 -0800

    Turing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 # This script creates a file in /tmp specifying the owner and permissions
 file { '/tmp/school':
+  ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
