d13735724db719491d0111fa8a035e7a4b0edb6b
Lwimix <martilwinx@gmail.com>
2024-03-26 11:05:13 +0300

    Fixed Nginx

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# This code replaces the extension of the settings file from .phpp to .php
-$setting = '/var/www/html/wp-settings.php'
-exec { 'replace_extension':
-command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' ${setting}",
-path    => ['/bin', '/usr/bin']
+# This code restarts nginx
+service { 'nginx':
+  ensure    => stopped,
+}
+
+exec { 'restart_nginx':
+  command  => '/usr/sbin/nginx',
+  provider => 'shell',
+  require  => Service['nginx'],
 }
